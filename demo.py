3260a9d82cc02f9fd580b4f5fedfe8d412e3ff43
DLP <dlp@dlp-CQ5700Y.(none)>
2014-04-05 15:21:25 -0400

    added to the main if at bottom

--- demo.py
+++ demo.py
@@ -56,4 +56,6 @@ app.add_url_rule('/', view_func=View.as_view('main'), methods=['GET', 'POST'])
 
 app.debug = True
 if __name__ == "__main__":
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
